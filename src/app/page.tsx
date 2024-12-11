963bc42fc415b8af443a752bde1f9afbc4a3c1ed
Umendran <125129294+Umen14@users.noreply.github.com>
2024-12-11 16:16:58 +0800

    mobile-support first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const ProjectCarousel = dynamic(() => import('@/components/Slider/page'), {
 
 // Custom button component
 const CustomButton: React.FC<{ text: string }> = React.memo(({ text }) => (
-  <Button className='hover:scale-110 transition-transform duration-300'>
+  <Button className="hover:scale-110 transition-transform duration-300">
     {text}
   </Button>
 ));
@@ -22,39 +22,39 @@ const CustomButton: React.FC<{ text: string }> = React.memo(({ text }) => (
 const Home: React.FC = () => {
   return (
     <motion.div
-      className='animate-slideUp overflow-hidden'
+      className="overflow-hidden animate-slideUp"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
       {/* About and Resume Section */}
-      <section className='text-3xl pl-[500px]'>
-        <div className='grid grid-cols-2 justify-between mt-60 mb-40 font-mono'>
-          <Link href='/about' passHref>
-            <CustomButton text='./ about \.' />
+      <section className="text-3xl pt-40 md:pt-60 pb-10 font-mono ">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-80 text-center">
+          <Link href="/about" passHref>
+            <CustomButton text="./ about \." />
           </Link>
-          <Link href='/resume' passHref>
-            <CustomButton text='./ resume \.' />
+          <Link href="/resume" passHref>
+            <CustomButton text="./ resume \." />
           </Link>
         </div>
       </section>
 
       {/* Introduction Section */}
-      <section className='text-center'>
-        <h1 className='text-5xl font-bold font-serif'>Hey There, Nice to Meet You</h1>
-        <div className='text-4xl font-mono mt-2'>
-          I’m <Typewriter text='Umendran, a Software Engineer I' speed={50} />
+      <section className="text-center pt-10 pb-10">
+        <h1 className="text-5xl font-bold font-serif">Hey There, Nice to Meet You</h1>
+        <div className="text-4xl font-mono mt-2">
+          I’m <Typewriter text="Umendran, a Software Engineer I" speed={50} />
         </div>
       </section>
 
       {/* Projects and Contact Section */}
-      <section className='text-3xl font-mono pl-[500px]'>
-        <div className='grid grid-cols-2 justify-between mt-40'>
-          <Link href='/projects' passHref>
-            <CustomButton text='./ projects \.' />
+      <section className="text-3xl pt-20 pb-10 font-mono">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-80 text-center">
+          <Link href="/projects" passHref>
+            <CustomButton text="./ projects \." />
           </Link>
-          <Link href='/contact' passHref>
-            <CustomButton text='./ contact \.' />
+          <Link href="/contact" passHref>
+            <CustomButton text="./ contact \." />
           </Link>
         </div>
       </section>
